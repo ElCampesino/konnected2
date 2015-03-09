a7b5021ecabe4b6f54ce802fa032e32338ef5ffe
Alejandro Golob <alejandrogolob@gmail.com>
2015-03-09 17:33:38 +0000

    Solve exercises 3.1 and 3.2

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,22 +1,32 @@
 require 'test_helper'
 
+  def setup
+    @base_title = "Konnected App"
+  end
+  
 class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Konnected App"
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | Konnected App"
+    assert_select "title", "Help | #{@base_title}"
   end
   
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | Konnected App"
+    assert_select "title", "About | #{@base_title}"
   end
 
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
+  end
+    
 end
